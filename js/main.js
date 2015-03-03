a4f9129022742780a3480f14e627d6e96530f5b9
Saurav S Kumar <orngee73@gmail.com>
2015-03-03 14:07:47 +0530

    asd

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(function(){
 
     jQuery(window).scroll(function () {
         if (jQuery(window).scrollTop() > 400) {
-            jQuery("#navigation").css("background-color","rgba(210, 50, 50, 0.9)");
+            jQuery("#navigation").css("background-color","rgba(210, 50, 50, 0.7)");
             jQuery("#navigation").addClass("animated-nav");
         } else {
             jQuery("#navigation").css("background-color","transparent");
