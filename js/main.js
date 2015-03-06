0f63c9e5c1f354477121859e9af2dfd3054730bd
Saurav S Kumar <orngee73@gmail.com>
2015-03-06 23:04:17 +0530

    testimonial

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(function(){
 
     jQuery(window).scroll(function () {
         if (jQuery(window).scrollTop() > 400) {
-            jQuery("#navigation").css("background-color","rgba(210, 50, 50, 0.99)");
+            jQuery("#navigation").css("background-color","rgba(210, 50, 50, 0.95)");
             jQuery("#navigation").addClass("animated-nav");
         } else {
             jQuery("#navigation").css("background-color","transparent");
